b7e7390c6d3973efe683165d322595d1cdfe73c2
Jamil <Jamilw284@gmail.com>
2025-10-05 13:32:32 -0500

    Added functionality to lock the desktop's screen

--- src/executor.py
+++ src/executor.py
@@ -1,9 +1,30 @@
-class Excecutor:
-    commands = {}
+import pyautogui
+import ctypes
+
+class Executor:
+    commands = {'open Notepad' : '',
+                'lock screen' : '',
+                'play media' : '',
+                'pause media' : '',}
 
     def validateCommand(command):
         return True
     
     def executeCommand(command):
         resp = commands[command]
+
+    def openNotepad(self):
+        return ""
     
+    def lockScreen(self):
+        ctypes.windll.user32.LockWorkStation()
+        print('Screen Lock Attempted')
+    def playMedia(self):
+        return ""
+
+    def pauseMedia(self):
+        return ""
+
+    
+execute = Executor()
+execute.lockScreen()
