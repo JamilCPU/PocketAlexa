1cc031af5ecb205fef7b15122c074542f5d20762
Jamil <Jamilw284@gmail.com>
2025-10-07 15:43:04 -0500

    Consolidated play/pause media into one function

--- src/executor.py
+++ src/executor.py
@@ -9,30 +9,25 @@ class Executor:
     def executeCommand(self, command):
         commands = {'open notepad' : self.openNotepad,
         'lock screen' : self.lockScreen,
-        'play media' : self.playMedia,
-        'pause media' : self.pauseMedia}
+        'play media' : self.playPauseMedia,
+        'pause media' : self.playPauseMedia}
         if command not in commands:
             return "EXECUTION ERROR\n 404 Not Found: '", command, "'"
 
-        print('Executing command... ', command)
+        print("Executing command... '", command, "'")
         resp = commands[command]()
-        print(resp)
         return resp
 
     def openNotepad(self):
         subprocess.Popen('notepad')
         print('notepad opened... returning statement')
-        return "Opened notepad"
+        return "SUCCESS: Opened Notepad"
     
     def lockScreen(self):
         ctypes.windll.user32.LockWorkStation()
-        return 'Screen Lock Attempted'
-    def playMedia(self):
-        time.sleep(1.5)
-        pyautogui.press('space')
-        return 'Playing active media'
+        return 'SUCCESS: Screen Locked'
 
-    def pauseMedia(self):
+    def playPauseMedia(self):
         time.sleep(1.5)
         pyautogui.press('space')
-        return 'Pausing active media'
+        return 'SUCCESS: Playing/Pausing Active Media'
